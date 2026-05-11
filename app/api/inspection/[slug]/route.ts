13f25c6c249e749a773e10112c5a992cbe009b33
Aldo Aldiero <rivelinoaldo@gmail.com>
2026-05-11 18:59:13 +0700

    fix: deployment error

--- app/api/inspection/[slug]/route.ts
+++ app/api/inspection/[slug]/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { returnInspectionDummy } from "@/lib/dummy/responseInpections";
 
-export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+export async function GET(request: NextRequest, { params }: { params: Promise<{ slug: string }> }) {
   try {
-  const { slug } = await params
+    const { slug } = await params
     const filteredData = returnInspectionDummy.filter(
       (item) => item.id === slug,
     );
